77a2421cca7c46aa16c9a7d3dacdd2f2b8ab4bfd
nikita <nikita.afanasev2020@gmail.com>
2018-05-14 23:45:55 +0800

    convert image to valid url in toArray

--- app/Element.php
+++ app/Element.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Element extends Model
 {
     protected $table = 'elements';
+
+    public function toArray($options = 0)
+    {
+        $json = parent::toArray();
+        $json['image'] = env('APP_URL') . Storage::url($json['image']);
+
+        return $json;
+    }
 }
--- app/Http/Controllers/API/ElementController.php
+++ app/Http/Controllers/API/ElementController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-// use Illuminate\Support\Facades\Storage;
 
 class ElementController extends Controller
 {
