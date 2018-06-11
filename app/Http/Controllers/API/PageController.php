1763ad78b2f3e380b53440eb50d39100d49360e9
nikita <nikita.afanasev2020@gmail.com>
2018-06-11 21:36:37 +0800

    include user name in bridges/notes for api search/page, #53

--- app/Http/Controllers/API/PageController.php
+++ app/Http/Controllers/API/PageController.php
@@ -17,7 +17,7 @@ class PageController extends Controller
             return $this->apiErr(22010, 'url is required');
         }
 
-        $result = $this->pageInfo($url, $user);
+        $result = $this->pageInfo($url, $user, true);
         return $this->apiOk($result);
     }
 
@@ -33,20 +33,42 @@ class PageController extends Controller
         return $this->apiOk($results);
     }
 
-    private function pageInfo($url, $user) {
+    private function pageInfo($url, $user, $withCreatorInfo = false) {
         $elements = \App\Element::where([ 'status' => 0, 'url' => $url ])->get();
         $eids     = array_map(function ($v) { return $v['id']; }, $elements->toArray());
 
         $notesQuery = \App\Note::where([ 'status' => 0 ])->whereIn('target', $eids);
         $notesQuery = $this->withPrivacyWhere($notesQuery, $user);
-        $notes      = $notesQuery->get();
+        $notes      = $notesQuery->get()->toArray();
 
         $bridgesQuery = \App\Bridge::where([ 'status' => 0 ])
                                     ->where(function($query) use($eids)  {
                                         $query->whereIn('from', $eids)->orWhereIn('to', $eids);
                                     });
         $bridgesQuery = $this->withPrivacyWhere($bridgesQuery, $user);
-        $bridges      = $bridgesQuery->get();
+        $bridges      = $bridgesQuery->get()->toArray();
+
+        if ($withCreatorInfo) {
+            $uids = array_merge(
+                array_map(function($note) { return $note['created_by']; }, $notes),
+                array_map(function($bridge) { return $bridge['created_by']; }, $bridges)
+            );
+            $users      = \App\User::whereIn('id', $uids)->get()->toArray();
+            $userMap    = array_reduce($users, function($prev, $user) {
+                $prev[$user['id']] = $user;
+                return $prev;
+            }, []);
+            
+            $notes = array_map(function ($note) use($userMap) {
+                $note['created_by_username'] = $userMap[$note['created_by']]['name'];
+                return $note;
+            }, $notes);
+
+            $bridges = array_map(function ($bridge) use($userMap) {
+                $bridge['created_by_username'] = $userMap[$bridge['created_by']]['name'];
+                return $bridge;
+            }, $bridges);
+        }
 
         return [
             'elements'  => $elements,
